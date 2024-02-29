aee7da1c0ca1961b7a764ad524a99c59ce30ef39
Seventeen <55933235+Steven-cpp@users.noreply.github.com>
2024-02-29 01:36:43 +0000

    feat: update prd data

--- pages/4_Insightor_Demo.py
+++ pages/4_Insightor_Demo.py
@@ -27,163 +27,154 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import yaml
 
-WEIGHT_MAP = {
-    'QualityFactor': 'Quality_Factor_Normalized',
-    'EmployeeNumber': 'EmployeeNumber_normalized',
-    'Revenue1YearTotal': 'Revenue1YearTotal_normalized',
-    'StrategySizeDistance': 'TargetAgeDistance_normalized',
-    'StrategyAgeDistance': 'TargetSizeDistance_normalized',
-    'Revenue2YearGrowthRate': 'Revenue2YearGrowthRate_normalized',
-    'Revenue3YearGrowthRate': 'Revenue3YearGrowthRate_normalized',
-    'PitchbookSizeMultiple': 'PitchbookSizeMultiple_normalized',
-    'PitchbookGrowthRate': 'PitchbookGrowthRate_normalized',
-    'ManagerDensity': 'ManagerDensity_normalized',
-    'ManagerQuality': 'ManagerQuality_normalized',
-    'ManagerNumber': 'ManagerNumber_normalized',
-    'EmployeeEstimatedGrowthRate': 'EmployeeEstimatedGrowthRate_normalized',
-    'KeyDevelopmentNumber': 'KeyDevelopmentNumber_normalized',
-    'WeightedKeyDevelopmentNumber': 'WeightedKeyDevelopmentNumber_normalized',
-    'ManagerDataAvailable': 'ManagerDataAvailable_normalized',
-    'IndustryNFit': 'IndustryNFit_normalized',
-    'DealStageNFit': 'DealStageNFit_normalized',
-    'FundraiseInactivity': 'FundraiseInactivity_normalized',
-    'FounderScore': 'FounderScore_normalized',
-    'RaisedBeyondAvg': 'RaisedBeyondAvg_normalized',
-    'ManagerQualityEst': 'ManagersQualityEst_normalized',
-    'ManagerQualityTrendEst': 'ManagersQualityTrendEst_normalized',
-    'GrossMarginInd': 'GrossMarginInd_normalized',
-    'EBITDAMarginInd': 'EBITDAMarginInd_normalized',
-    'EBITMarginInd': 'EBITMarginInd_normalized',
-    'LeverageInd': 'LeverageInd_normalized',
-    'RevenueInd': 'RevenueInd_normalized',
-    'RevenueGrowthInd': 'RevenueGrowthInd_normalized'
-}
-WEIGHT_COL  = WEIGHT_MAP.keys()
-SCORING_COL  = WEIGHT_MAP.values()
+# Function to load configurations from a YAML file
+def load_config(config_path):
+    with open(config_path, "r", encoding='ascii') as file:
+        cfg = yaml.safe_load(file)
+    return cfg
 
-df_weights = pd.read_csv('./data/scorer_weights.csv')
-df = pd.read_csv('./data/scorer_debug_merged_v2.csv')
-df = df.iloc[:, 2:]
+config = load_config("./config.yaml")
+WEIGHT_MAP = config["WEIGHT_MAP"]
+STRATEGY_MAP = config["STRATEGY_MAP"]
 
-# Sidebar - Navigation Bar
-st.sidebar.title("Navigation Bar")
-score_select = st.sidebar.selectbox("Select Score", options=['ScoreP1', 'ScoreP3'])
+FN_BM_PATTERN = config["FN_BM_PATTERN"]
+DATA_DIR = config["DATA_DIR"]
+FN_SCORER_WEIGHTS = config["FN_SCORER_WEIGHTS"]
+bm_list = sorted(map(lambda x: x.split('/')[-1], glob.glob(DATA_DIR + FN_BM_PATTERN)))
 
-STRATEGYNAME_COL = "StrategyName"
-STRATEGY_MAP = {"ScoreP1": 0, "ScoreP3": 1}
+# Load data
+@st.cache_data
+def load_data():
+    df_weights = pd.read_csv(DATA_DIR + FN_SCORER_WEIGHTS)
+    df_weights.fillna(0, inplace=True)
+    df = pd.read_csv(DATA_DIR + 'scorer_debug_merged_prd_v2.csv', index_col='Id').iloc[:, 1:]
+    # df = pd.read_csv(DATA_DIR + bm_list[-1], index_col='Id').iloc[:, 1:]
+    return df, df_weights
 
-# Display sliders for weights, default values from `weights` list
-weightMap = df_weights.loc[STRATEGY_MAP[score_select]].drop(STRATEGYNAME_COL).astype(float).items()
-# weightMap = sorted(weightMap, key=lambda item: abs(item[1]), reverse=True)
-scorer_col = []
-for weight, value in weightMap:
-    scorer_col.append(WEIGHT_MAP[weight])
-    if value >= 0:
-        df_weights.loc[STRATEGY_MAP[score_select], weight] = st.sidebar.slider(weight, min_value=0.0, max_value=3.0, value=value, step=0.01)
-    else:
-        df_weights.loc[STRATEGY_MAP[score_select], weight] = st.sidebar.slider(weight, min_value=-1.0, max_value=0.0, value=value, step=0.01)
+# Sidebar configuration
+def configure_sidebar(df_weights):
+    st.sidebar.title("Navigation Bar")
+    score_type = st.sidebar.selectbox("Select Score", options=['ScoreP1', 'ScoreP3'])
 
-scoring = df[SCORING_COL].fillna(0).to_numpy()
-weights = df_weights.loc[STRATEGY_MAP[score_select], WEIGHT_COL].to_numpy().astype(float)
-scoreDebug = np.dot(scoring, weights)/10
+    weightMap = df_weights.loc[STRATEGY_MAP[score_type]].drop("StrategyName").astype(float).items()
+    for weight, value in weightMap:
+        if weight in WEIGHT_MAP:
+            if value >= 0:
+                df_weights.loc[STRATEGY_MAP[score_type], weight] = st.sidebar.slider(weight, min_value=0.0, max_value=3.0, value=value, step=0.01)
+            else:
+                df_weights.loc[STRATEGY_MAP[score_type], weight] = st.sidebar.slider(weight, min_value=-1.0, max_value=0.0, value=value, step=0.01)
+    return score_type
 
-df['ScoreDebug'] = scoreDebug
+@st.cache_data
+def calculate_scores(df, df_weights, score_type):
+    """Calculate ScoreDebug and BM difference
 
-col_default = ['CompanyName', 'PrimaryIndustry', 'Score', 'ScoreDebug', 'BusinessDescription']
+    Args:
+        df (_type_): _description_
+        df_weights (_type_): _description_
+        score_type (_type_): _description_
+    """
+    SCORING_COL = WEIGHT_MAP.values()
+    scoring = df[SCORING_COL].fillna(0).to_numpy()
+    weights = df_weights.loc[STRATEGY_MAP[score_type], WEIGHT_MAP.keys()].to_numpy().astype(float)
+    scoreDebug = np.dot(scoring, weights)
+    scoreDebug = (scoreDebug / df['Quality_Factor']) + \
+            (df_weights.loc[STRATEGY_MAP[score_type], 'QualityFactor'] * df['Quality_Factor_Normalized'])
+    df['ScoreDebug'] = scoreDebug
+    return df
 
-# Buttons in sidebar
-if st.sidebar.button('SaveAsBM'):
-    st.sidebar.write('SaveAsBM clicked')
-if st.sidebar.button('AutoFT'):
-    st.sidebar.write('AutoFT clicked')
+@st.cache_data
+def calculate_bm(df, bm, prefix_delta):
+    SCORING_COL = WEIGHT_MAP.values()
+    DELTA_COL = list(SCORING_COL)
+    DELTA_COL.append('Score')
+    
+    df_delta = None
+    
+    if bm is not None:
+        df_bm = pd.read_csv(DATA_DIR + bm, index_col='Id')
+        scoring, df_bm_aligned = df[DELTA_COL].align(df_bm, join='left', axis=0, fill_value=0)
+        df_delta = scoring - df_bm_aligned[DELTA_COL].fillna(0).to_numpy()
+        df_delta.columns = [f'{prefix_delta}{col}' for col in df_delta.columns]
+    return df_delta
+    
 
-# Main Page
-st.title("Score Debugging App")
-
-dir = './data/'
-fname = 'scorer_debug_merged*'
-# List all files in the current directory starting with 'merged'
-bm_list = map(lambda x: x.split('/')[-1], glob.glob(dir + fname))
-
-bm = st.selectbox(
-    'Which benchmark you would like to compare',
-    bm_list, index=None)
-
-# Multi-select for columns to show
-options = st.multiselect('Select columns to show', df.columns, default=col_default)
-
-
-
-# Display dataframe with selected columns
-st.dataframe(df[options])
-
-data = df.melt(id_vars=['CompanyName'],
+def plot_score_dist(df, score_type):
+    data = df.melt(id_vars=['CompanyName'],
                value_vars=['Score', 'ScoreDebug'],
                var_name='ScoreType',
-               value_name=score_select)
+               value_name=score_type)
+    # Plotting the score distribution with Altair
+    chart = alt.Chart(data).mark_bar(opacity=0.3).encode(
+        alt.X(f'{score_type}:Q', bin=alt.Bin(maxbins=40)),
+        y=alt.Y('count()', stack=None),
+        color='ScoreType:N'
+    ).properties(
+        title='Score Distribution'
+    )
+    st.altair_chart(chart, use_container_width=True)
 
-# Plotting the score distribution with Altair
-chart = alt.Chart(data).mark_bar(opacity=0.3).encode(
-    alt.X(f'{score_select}:Q', bin=alt.Bin(maxbins=40)),
-    y=alt.Y('count()', stack=None),
-    color='ScoreType:N'
-).properties(
-    title='Score Distribution'
-)
-st.altair_chart(chart, use_container_width=True)
+def plot_score_comp(df, cols):
+    df_fin = df[cols].astype(float)
+    data_binned = pd.DataFrame()
+
+    # Bin the data into 5 bins for each score
+    bins = 5
+    labels = ['< 0.2', '[0.2, 0.4)', '[0.4, 0.6)', '[0.6, 0.8)', '>= 0.8']
+    for column in df_fin.columns:
+        if (pd.notna(df_fin[column]).sum() < bins):
+            continue
+        data_binned[column + "_bin"] = pd.cut(df_fin[column].to_numpy(), bins, labels=[labels[i] for i in range(bins)])
+
+    # Transform the data to a long format suitable for Altair
+    df_long = pd.melt(data_binned, value_vars=[column for column in data_binned.keys()], var_name='ScoreType', value_name='Bin')
+
+    # Create a stacked bar chart
+    barChart = alt.Chart(df_long).mark_bar().encode(
+        x='ScoreType:N',
+        y=alt.Y('count()', stack='zero'),
+        color='Bin:N'
+    ).properties(
+        height=500,
+        title='Key Financials Composition'
+    )
+
+    st.altair_chart(barChart, use_container_width=True)
 
 
-NEW_COLS = ['GrossMarginInd_normalized', 'EBITDAMarginInd_normalized', 'EBITMarginInd_normalized', 'LeverageInd_normalized', 'RevenueInd_normalized', 'RevenueGrowthInd_normalized']
-data_comp = df[NEW_COLS].astype(float)
-data_binned = pd.DataFrame()
+def plot_corr_mat():
+    pass
 
-# Bin the data into 5 bins for each score
-bins = 5
-labels = ['< 0.2', '[0.2, 0.4)', '[0.4, 0.6)', '[0.6, 0.8)', '>= 0.8']
-for column in data_comp.columns:
-    if (pd.notna(data_comp[column]).sum() < bins):
-        continue
-    data_binned[column + "_bin"] = pd.cut(data_comp[column].to_numpy(), bins, labels=[labels[i] for i in range(bins)])
+# Main page configuration
+def configure_main_page(df, df_weights, score_type, prefix_delta='Δ'):
+    st.title("Score Debugging App")
+    bm = st.selectbox( 'Select benchmark you would like to compare', bm_list)
+    df = calculate_scores(df, df_weights, score_type)
+    df_delta = calculate_bm(df, bm, prefix_delta)
+    col_default = ['CompanyName', 'PrimaryIndustry', 'Score', 'ScoreDebug', 'BusinessDescription']
+    options = st.multiselect('Select columns to show', df.columns, default=col_default)
+    
+    if df_delta is not None:
+        col_delta = [f'{prefix_delta}{i}' for i in options if f'{prefix_delta}{i}' in df_delta.columns]
+        df_res = pd.merge(df[options], df_delta[col_delta], how='left', left_index=True, right_index=True)
+        st.dataframe(df_res)
+    else:
+        st.dataframe(df[options])
+        
+    plot_score_dist(df, score_type)
+    
+    KEY_FIN_COLS = ['GrossMarginInd_normalized', 'EBITDAMarginInd_normalized', 'EBITMarginInd_normalized', 'LeverageInd_normalized', 'RevenueInd_normalized', 'RevenueGrowthInd_normalized']
+    plot_score_comp(df, KEY_FIN_COLS)
+    
 
-# Transform the data to a long format suitable for Altair
-df_long = pd.melt(data_binned, value_vars=[column for column in data_binned.keys()], var_name='ScoreType', value_name='Bin')
+# Main function to run the app
+def main():
+    df, df_weights = load_data()
+    score_select = configure_sidebar(df_weights)
+    configure_main_page(df, df_weights, score_select)
 
-# # Count the occurrences in each bin for each score type
-# df_long['Count'] = 1
-# df_grouped = df_long.groupby(['ScoreType', 'Bin']).count().reset_index()
 
-# Create a stacked bar chart
-barChart = alt.Chart(df_long).mark_bar().encode(
-    x='ScoreType:N',
-    y=alt.Y('count()', stack='zero'),
-    color='Bin:N'
-).properties(
-    height=500,
-    title='Key Financials Composition'
-)
-
-st.altair_chart(barChart, use_container_width=True)
-
-## creating AgGrid dynamic table and setting configurations
-# gb = GridOptionsBuilder.from_dataframe(df_filtered)
-# gb.configure_selection(selection_mode="single", use_checkbox=True)
-# gb.configure_column(field='Company Name', width=270)
-# gb.configure_column(field='Sector', width=260)
-# gb.configure_column(field='Industry', width=350)
-# gb.configure_column(field='Prospect Status', width=270)
-# gb.configure_column(field='Product', width=240)
-
-# gridOptions = gb.build()
-
-# response = AgGrid(
-#     df_filtered,
-#     gridOptions=gridOptions,
-#     enable_enterprise_modules=False,
-#     height=600,
-#     update_mode=GridUpdateMode.SELECTION_CHANGED,
-#     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-#     fit_columns_on_grid_load=False,
-#     theme='alpine',
-#     allow_unsafe_jscode=True
-# )
+if __name__ == "__main__":
+    main()
